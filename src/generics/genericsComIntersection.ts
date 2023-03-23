de2008ad0d9c4f00c49fa52373188923edaed4bf
1mmanuelKant <joselisboaa05@gmail.com>
2023-03-23 19:38:49 -0300

    type predicate

--- src/generics/genericsComIntersection.ts
+++ src/generics/genericsComIntersection.ts
@@ -1,11 +1,11 @@
 type mergeObjectsFn = <T, U>(obj1: T, obj2: U) => typeof obj1 & typeof obj2;
 
 const mergeObjects: mergeObjectsFn = (obj1, obj2) => {
-    return { ...obj1, ...obj2 }
-}
+  return { ...obj1, ...obj2 };
+};
 
 function mergeObjects2<T, U>(fn: mergeObjectsFn, obj1: T, obj2: U) {
-    console.log(fn( obj1, obj2 ))
+  console.log(fn(obj1, obj2));
 }
 
-mergeObjects2(mergeObjects, {nome: "elisson"}, {sobrenome: "Saldanha"})
+mergeObjects2(mergeObjects, { nome: "elisson" }, { sobrenome: "Saldanha" });
